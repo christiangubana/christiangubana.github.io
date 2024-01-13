0fd351f82fa55b461a6c7ca13b963bdc78dec60b
christiangubana <christiangubana@gmail.com>
2024-01-13 13:01:38 +0200

    configured the app for deployment

--- next.config.js
+++ next.config.js
@@ -4,4 +4,6 @@ module.exports = {
     domains: ["avatars.githubusercontent.com"],
   },
   output: "standalone",
+  basePath: "/christiangubana.github.io", // Replace 'repo-name' with your actual repository name
+  assetPrefix: "/christiangubana.github.io/", // Replace 'repo-name' with your actual repository name
 };
