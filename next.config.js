0fa6bbe5a206d303c1d05254c40af646462f5cfd
christiangubana <christiangubana@gmail.com>
2024-01-13 14:14:50 +0200

    update homepage url from package.json file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  reactStrictMode: false,
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-  },
-  output: "standalone",
+  // reactStrictMode: false,
+  // images: {
+  //   domains: ["avatars.githubusercontent.com"],
+  // },
+  // output: "standalone",
   basePath: "/christiangubana.github.io", // Replace 'repo-name' with your actual repository name
   assetPrefix: "/christiangubana.github.io/", // Replace 'repo-name' with your actual repository name
 };
